72df188e5e7f0792333586f4a3fbd6d02631b0d2
Steven Cedro <steven@southbanksoftware.com>
2020-12-02 09:53:28 +1100

    Added Copyright notices.

--- adapter_test.go
+++ adapter_test.go
@@ -1,5 +1,7 @@
 // Copyright 2018 The casbin Authors. All Rights Reserved.
 //
+// Copyright 2020 Southbank Software Pty Ltd. All Rights Reserved.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
